dcae91eda33696f832fdb00cdaceb9a762742fff
Feilli <kirill.pilipchuk@gmail.com>
2017-02-10 17:36:14 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,20 @@
-var arr = [
-	[2, 0, 2],
-	[0, 2, 0],
-	[2, 0, 2]
-]
+// Field size
+const FIELD_SIZE = 3;
 
+// Cell status
+const EMPTY = 0;
+const CROSS = 1;
+const CIRCLE = 2;
+
+var Cells;
+var Arr = [];
 
 function WinCondition()
 {
-	// var Cells = document.getElementsByClassName("cell");
-
-	for(var i = 0; i < arr.length; i++)
-	{
-		
-	}
 	for (var i=0; i<3; i++) {
 		for(var j=0; j<3; j++){
-			if (arr[i][j]!=0) 
-				console.log(Derevo(arr[i][j], i, j));
+			if (Arr[i][j]!=0) 
+				console.log(Derevo(Arr[i][j], i, j));
 		}
 	}
 }
@@ -31,7 +29,7 @@ function Derevo(key, Row, Column){
 	nRow=Row;
 	nColumn=Column;
 	for (var i =0; i<3; i++){
-		if ((nRow+dRow<=3) && (nColumn+dColumn>=-1) && (arr[nRow][nColumn]==key)) { 
+		if ((nRow+dRow<=3) && (nColumn+dColumn>=-1) && (Arr[nRow][nColumn]==key)) { 
 			nRow+=dRow;
 			nColumn+=dColumn;
 		}
@@ -46,7 +44,7 @@ function Derevo(key, Row, Column){
 	nRow=Row;
 	nColumn=Column;
 	for (var i =0; i<3; i++){
-		if ((nRow+dRow<=3) && (arr[nRow][nColumn]==key)) { 
+		if ((nRow+dRow<=3) && (Arr[nRow][nColumn]==key)) { 
 			nRow+=dRow;
 			nColumn+=dColumn;
 		}
@@ -61,7 +59,7 @@ function Derevo(key, Row, Column){
 	nRow=Row;
 	nColumn=Column;
 	for (var i =0; i<3; i++){
-		if ((nRow+dRow<=3) && (nColumn+dColumn<=3) && (arr[nRow][nColumn]==key)) { 
+		if ((nRow+dRow<=3) && (nColumn+dColumn<=3) && (Arr[nRow][nColumn]==key)) { 
 			nRow+=dRow;
 			nColumn+=dColumn;
 		}
@@ -76,7 +74,7 @@ function Derevo(key, Row, Column){
 	nRow=Row;
 	nColumn=Column;
 	for (var i =0; i<=3; i++){
-		if ((nColumn+dColumn<=3) && (arr[nRow][nColumn]==key)) {
+		if ((nColumn+dColumn<=3) && (Arr[nRow][nColumn]==key)) {
 
 			nRow+=dRow;
 			nColumn+=dColumn;
@@ -90,4 +88,53 @@ function Derevo(key, Row, Column){
 	 return 0;
 }
 
-WinCondition();
+function GetField()
+{
+	Cells = document.getElementsByTagName("td");
+
+	var k = 0;
+
+	for(var i = 0; i < FIELD_SIZE; i++)
+	{
+		Arr[i] = [];
+
+		for(var j = 0; j < FIELD_SIZE; j++)
+		{
+			switch(Cells[k].innerHTML)
+			{
+				case 'X' :
+					Arr[i][j] = CROSS;
+				break;
+
+				case 'O' : 
+					Arr[i][j] = CIRCLE;
+				break;
+
+				default:
+					Arr[i][j] = EMPTY;
+			}
+
+			k++;
+		}
+	}
+}
+
+function WriteField()
+{
+	for(var i = 0; i < 3; i++)
+	{
+		var str = "";
+
+		for(var j = 0; j < 3; j++)
+		{
+			str = str + " " + Arr[i][j];
+		}
+
+		console.log(str);
+	}
+}
+
+// After our window wal loaded
+window.onload = function () {
+	GetField();
+}
